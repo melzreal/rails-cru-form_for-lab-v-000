da809d67bbef3210870fb2f293fc40cf642f3f57
Melissa Real <melissa.real@hotmail.com>
2019-01-06 15:35:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params.require(:song).permit(:name, :artist_id, genre_id)))
+    @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id)))
     @song.save
     redirect_to song_path(@song)
   end
