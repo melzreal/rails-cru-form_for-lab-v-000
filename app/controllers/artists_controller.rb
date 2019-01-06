57b24166018b7ee5746ec6326874887e8fd375b2
Melissa Real <melissa.real@hotmail.com>
2019-01-06 15:35:25 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -24,7 +24,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(params.require(:artist))
+    @artist.update(params.require(:artist).permit(:name, :bio))
     redirect_to artist_path(@artist)
   end
 end
